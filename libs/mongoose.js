8f74d9611a5e027d7bc4a3db8ecef33f95439741
Mekitoji <Mekitojizzz@gmail.com>
2014-07-07 16:41:32 +0400

    add mongoose connection log

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -4,4 +4,14 @@ var config = require('../config');
 
 mongoose.connect(config.get('mongoose:uri'), config.get('mongoose: option'));
 
+var db = mongoose.connection;
+var log = require('./log');
+
+db.on('error', function(err) {
+  log.error('connection error:', err.message);
+});
+db.once('open', function callback() {
+  log.info("Connected to DB!");
+});
+
 module.exports = mongoose;
