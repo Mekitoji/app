c85a0cdd33e8401c8b463eeb6ab7bf494a5354f7
Mekitoji <Mekitojizzz@gmail.com>
2014-07-09 17:36:08 +0400

    .

---
+++ models/calendar.js
@@ -0,0 +1,16 @@
+var mongoose = require('../libs/mongoose');
+
+module.exports = mongoose.model('Calendar', {
+  appName: {
+    //object appName from 'Apps'
+    type: String
+  },
+  Storage: {
+    date: {
+      type: Date
+    },
+    value: {
+      type: String
+    }
+  }
+});
--- public/js/new-core.js
+++ public/js/new-core.js
@@ -1,99 +1,123 @@
 angular.module('project', ['ngRoute'])
 //restful 
 .factory('Apps', function($http) {
-    return {
-        get: function() {
-            return $http.get('/api/gk');
-        },
-        create: function(appData) {
-            return $http.post('/api/gk', appData);
-        },
-        update: function(id, appData) {
-            return $http.put('/api/gk/' + id, appData);
-        },
-        delete: function(id) {
-            return $http.delete('api/gk' + id);
-        }
+  return {
+    get: function() {
+      return $http.get('/api/gk');
+    },
+    create: function(appData) {
+      return $http.post('/api/gk', appData);
+    },
+    update: function(id, appData) {
+      return $http.put('/api/gk/' + id, appData);
+    },
+    delete: function(id) {
+      return $http.delete('api/gk' + id);
     }
+  }
 })
 
 .config(function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            controller: 'ListCtrl',
-            templateUrl: 'list.html'
-        })
-        .when('/edit/:projectId', {
-            controller: 'EditCtrl',
-            templateUrl: 'detail.html'
-        })
-        .when('/new', {
-            controller: 'CreateCtrl',
-            templateUrl: 'detail.html'
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
+  $routeProvider
+    .when('/', {
+      controller: 'ListCtrl',
+      templateUrl: 'list.html'
+    })
+    .when('/edit/:appId', {
+      controller: 'EditCtrl',
+      templateUrl: 'detail.html'
+    })
+    .when('/new', {
+      controller: 'CreateCtrl',
+      templateUrl: 'detail.html'
+    });
+
 })
 
-.controller('ListCtrl', function($scope, Apps) {
-    $scope.projects = Apps;
+.controller('ListCtrl', function($scope, $http, Apps) {
+  Apps.get()
+
+  .success(function(data) {
+    $scope.apps = data;
+  });
 })
 
-.controller('CreateCtrl', function($scope, $location, $timeout, Projects) {
-    $scope.save = function() {
-        Projects.$add($scope.project, function() {
-            $timeout(function() {
-                $location.path('/');
-            });
-        });
-    };
+.controller('CreateCtrl', function($scope, $http, Apps) {
+  $scope.formData = {};
+  $scope.createApp = function() {
+    Apps.create($scope.formData)
+
+    .success(function(data) {
+      $scope.apps = data;
+      $scope.formData = {};
+    });
+  };
 })
 
-.controller('EditCtrl', function($scope, $location, $routeParams, $firebase, fbURL) {
-    var projectUrl = fbURL + $routeParams.projectId;
-    $scope.project = $firebase(new Firebase(projectUrl));
+.controller('EditCtrl', function($scope, $routeParams, $location, $http, Apps) {
 
-    $scope.destroy = function() {
-        $scope.project.$remove();
+  // var projectUrl = $routeParams.appId;
+  // Apps.update(projectUrl, $scope.formData)
+  //   .success(function(data) {
+  //     $scope.formData = data;
+
+  //   })
+
+  $scope.deleteApp = function(id) {
+    Apps.delete(id)
+      .success(function(data) {
+        $scope.apps = data; //get new list
         $location.path('/');
-    };
+      });
+  };
 
-    $scope.save = function() {
-        $scope.project.$save();
+  $scope.updateApp = function(id) {
+    Apps.update(id, $scope.formData)
+      .success(function(data) {
+        $scope.apps = data;
         $location.path('/');
-    };
+      });
+  };
+
 })
+
+
+
+
+
+
+
+
 
 .controller('mainController', function($scope, $http, Apps) {
-    //get formData clear
-    $scope.formData = {};
-    //get  all apps and show them
-    Apps.get()
-        .success(function(data) {
-            $scope.apps = data;
-        });
+  //get formData clear
+  $scope.formData = {};
+  //get  all apps and show them
+  Apps.get()
+    .success(function(data) {
+      $scope.apps = data;
+    });
 
-    $scope.createApp = function() {
-        Apps.create($scope.formData)
-            .success(function(data) {
-                $scope.apps = data;
-                $scope.formData = {};
-            });
-    };
+  $scope.createApp = function() {
+    Apps.create($scope.formData)
+      .success(function(data) {
+        $scope.apps = data;
+        $scope.formData = {};
+      });
+  };
 
-    $scope.deleteApp = function(id) {
-        Apps.delete(id)
-            .success(function(data) {
-                $scope.apps = data; //get new list
-            });
-    };
+  $scope.deleteApp = function(id) {
+    Apps.delete(id)
+      .success(function(data) {
+        $scope.apps = data; //get new list
+      });
+  };
 
-    $scope.updateApp = function(id) {
-        Apps.update(id, $scope.formData)
-            .success(function(data) {
-                $scope.apps = data;
-            });
-    };
+  $scope.updateApp = function(id) {
+    Apps.update(id, $scope.formData)
+      .success(function(data) {
+        $scope.apps = data;
+      });
+  };
 
 });
