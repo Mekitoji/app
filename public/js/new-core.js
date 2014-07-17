9e77c915f5ffbfec9a4f87940687d71586f291d4
Mekitoji <Mekitojizzz@gmail.com>
2014-07-17 15:12:17 +0400

    delete app button

--- public/js/new-core.js
+++ public/js/new-core.js
@@ -1,121 +1,123 @@
 angular.module('project', ['ngRoute'])
 //restful 
 .factory('Apps', function($http) {
-  return {
-    get: function() {
-      return $http.get('/api/gk');
-    },
-    create: function(appData) {
-      return $http.post('/api/gk', appData);
-    },
-    update: function(id, appData) {
-      return $http.put('/api/gk/' + id, appData);
-    },
-    delete: function(id) {
-      return $http.delete('api/gk' + id);
+    return {
+        get: function() {
+            return $http.get('/api/gk');
+        },
+        create: function(appData) {
+            return $http.post('/api/gk', appData);
+        },
+        update: function(id, appData) {
+            return $http.put('/api/gk/' + id, appData);
+        },
+        delete: function(id) {
+            return $http.delete('api/gk/' + id);
+        }
     }
-  }
 })
 
 .config(function($routeProvider) {
-  $routeProvider
-    .when('/', {
-      controller: 'ListCtrl',
-      templateUrl: 'list.html'
-    })
-    .when('/edit/:appId', {
-      controller: 'EditCtrl',
-      templateUrl: 'detail.html'
-    })
-    .when('/new', {
-      controller: 'CreateCtrl',
-      templateUrl: 'detail.html'
-    })
-    .when('/calendar', {
-      templateUrl: 'calendar.html'
-    })
-    .when('/test', {
-      templateUrl: 'test.html'
-    })
+    $routeProvider
+        .when('/', {
+            controller: 'ListCtrl',
+            templateUrl: 'list.html'
+        })
+        .when('/edit/:appId', {
+            controller: 'EditCtrl',
+            templateUrl: 'detail.html'
+        })
+        .when('/new', {
+            controller: 'CreateCtrl',
+            templateUrl: 'detail.html'
+        })
+        .when('/calendar', {
+            templateUrl: 'calendar.html'
+        })
+        .when('/test', {
+            templateUrl: 'test.html'
+        })
 
 })
 
 .controller('ListCtrl', function($scope, $http, Apps) {
-  Apps.get()
+    $scope.edit = false;
+    Apps.get()
 
-  .success(function(data) {
-    $scope.apps = data;
-  });
+    .success(function(data) {
+        $scope.apps = data;
+    });
 })
 
 .controller('CreateCtrl', function($scope, $http, Apps) {
-  $scope.formData = {};
-  $scope.createApp = function() {
-    Apps.create($scope.formData)
+    $scope.formData = {};
+    $scope.createApp = function() {
+        Apps.create($scope.formData)
 
-    .success(function(data) {
-      $scope.apps = data;
-      $scope.formData = {};
-    });
-  };
+        .success(function(data) {
+            $scope.apps = data;
+            $scope.formData = {};
+        });
+    };
 })
 
 .controller('EditCtrl', function($scope, $routeParams, $location, $http, Apps) {
 
-  var projectUrl = $routeParams.appId;
-  Apps.update(projectUrl, $scope.formData)
-    .success(function(data) {
-      $scope.formData = data;
+    $scope.edit = true;
+    var projectUrl = $routeParams.appId;
+    Apps.update(projectUrl, $scope.formData)
+        .success(function(data) {
+            $scope.formData = data;
 
-    })
+        })
 
-  $scope.deleteApp = function(id) {
-    Apps.delete(id)
-      .success(function(data) {
-        $scope.apps = data; //get new list
-        $location.path('/');
-      });
-  };
+    $scope.deleteApp = function(id) {
+        Apps.delete(id, $scope.formData)
+            .success(function(data) {
+                $scope.apps = data; //get new list
+                $location.path('/');
+            });
+    };
 
-  $scope.updateApp = function(id) {
-    Apps.update(id, $scope.formData)
-      .success(function(data) {
-        $scope.apps = data;
-        $location.path('/');
-      });
-  };
+    $scope.updateApp = function(id) {
+        Apps.update(id, $scope.formData)
+            .success(function(data) {
+                $scope.apps = data;
+                $location.path('/');
+            });
+    };
 
 })
 
 .controller('mainController', function($scope, $http, Apps) {
-  //get formData clear
-  $scope.formData = {};
-  //get  all apps and show them
-  Apps.get()
-    .success(function(data) {
-      $scope.apps = data;
-    });
+    //get formData clear
+    $scope.formData = {};
+    //get  all apps and show them
+    Apps.get()
+        .success(function(data) {
+            $scope.apps = data;
+        });
 
-  $scope.createApp = function() {
-    Apps.create($scope.formData)
-      .success(function(data) {
-        $scope.apps = data;
-        $scope.formData = {};
-      });
-  };
+    $scope.createApp = function() {
+        Apps.create($scope.formData)
+            .success(function(data) {
+                $scope.apps = data;
+                $scope.formData = {};
+            });
+    };
 
-  $scope.deleteApp = function(id) {
-    Apps.delete(id)
-      .success(function(data) {
-        $scope.apps = data; //get new list
-      });
-  };
+    $scope.deleteApp = function(id) {
+        Apps.delete(id)
+            .success(function(data) {
+                $scope.apps = data; //get new list
+            });
+    };
 
-  $scope.updateApp = function(id) {
-    Apps.update(id, $scope.formData)
-      .success(function(data) {
-        $scope.apps = data;
-      });
-  };
+    $scope.updateApp = function(id) {
+        Apps.update(id, $scope.formData)
+            .success(function(data) {
+                $scope.apps = data;
+            });
+    };
 
 });
--- routes/api_gk.js
+++ routes/api_gk.js
@@ -2,104 +2,104 @@ var Apps = require('../models/gkbase');
 
 module.exports = function(app) {
 
-  // api ---------------------------------------------------------------------
-  app.get('/api/gk', function(req, res) {
+    // api ---------------------------------------------------------------------
+    app.get('/api/gk', function(req, res) {
 
-    // use mongoose to get all gk in the database
-    Apps.find(function(err, app) {
+        // use mongoose to get all gk in the database
+        Apps.find(function(err, app) {
 
-      // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-      if (err)
-        res.send(err)
+            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+            if (err)
+                res.send(err)
 
-      res.json(app); // return all users in JSON format
-    });
-  });
-
-  // create user and send back all users after creation
-  app.post('/api/gk', function(req, res) {
-
-    // create a user, information comes from AJAX request from Angular
-    Apps.create({
-      country: req.body.country,
-      appName: req.body.appName,
-      category: req.body.category,
-      sdpStatus: req.body.sdpStatus,
-      updateTime: req.body.updateTime,
-      seller: req.body.seller,
-      tv: req.body.tv,
-      currentStatus: req.body.currentStatus,
-      testCycles: req.body.testCycles,
-      replyTime: req.body.replyTime,
-      resp: req.body.resp
-    }, function(err, app) {
-      if (err)
-        res.send(err);
-
-      // get and return all the users after you create another
-      Apps.find(function(err, apps) {
-        if (err)
-          res.send(err)
-        res.json(apps);
-      });
+            res.json(app); // return all users in JSON format
+        });
     });
 
-  });
+    // create user and send back all users after creation
+    app.post('/api/gk', function(req, res) {
 
-  // delete a user
-  app.delete('/api/gk/:app_id', function(req, res) {
-    Apps.remove({
-      _id: req.params.app_id
-    }, function(err, app) {
-      if (err)
-        res.send(err);
+        // create a user, information comes from AJAX request from Angular
+        Apps.create({
+            country: req.body.country,
+            appName: req.body.appName,
+            category: req.body.category,
+            sdpStatus: req.body.sdpStatus,
+            updateTime: req.body.updateTime,
+            seller: req.body.seller,
+            tv: req.body.tv,
+            currentStatus: req.body.currentStatus,
+            testCycles: req.body.testCycles,
+            replyTime: req.body.replyTime,
+            resp: req.body.resp
+        }, function(err, app) {
+            if (err)
+                res.send(err);
 
-      // get and return all the users after you create another
-      Apps.find(function(err, apps) {
-        if (err)
-          res.send(err)
-        res.json(apps);
-      });
-    });
-  });
-
-  //update a user
-  app.put('/api/gk/:app_id', function(req, res) {
-
-    // use our bear model to find the bear we want
-    Apps.findById(req.params.app_id, function(err, app) {
-
-      if (err) res.send(err);
-      //put some data for update here
-      if (req.body.country) app.country = req.body.country;
-      if (req.body.appName) app.appName = req.body.appName;
-      if (req.body.category) app.category = req.body.category;
-      if (req.body.sdpStatus) app.sdpStatus = req.body.sdpStatus;
-      if (req.body.updateTime) app.updateTime = req.body.updateTime;
-      if (req.body.seller) app.seller = req.body.seller;
-      if (req.body.tv) app.tv = req.body.tv;
-      if (req.body.currentStatus) app.currentStatus = req.body.currentStatus;
-      if (req.body.testCycles) app.testCycles = req.body.testCycles;
-      if (req.body.replyTime) app.replyTime = req.body.replyTime;
-      if (req.body.resp) app.resp = req.body.resp;
-      app._id = req.body._id; //  $id wont work, so use this id
-
-      // save the bear
-      app.save(function(err) {
-        if (err)
-          res.send(err);
-
-        res.json(app);
-      });
+            // get and return all the users after you create another
+            Apps.find(function(err, apps) {
+                if (err)
+                    res.send(err)
+                res.json(apps);
+            });
+        });
 
     });
-  });
 
-  // application -------------------------------------------------------------
-  //add emitter for development and production env
-  app.get('*', function(req, res) {
-    res.render('index.ejs'); // load the single view file (angular will handle the page changes on the front-end)
-  });
+    // delete a user
+    app.delete('/api/gk/:app_id', function(req, res) {
+        Apps.remove({
+            _id: req.params.app_id
+        }, function(err, app) {
+            if (err)
+                res.send(err);
+
+            // get and return all the users after you create another
+            Apps.find(function(err, apps) {
+                if (err)
+                    res.send(err)
+                res.json(apps);
+            });
+        });
+    });
+
+    //update a user
+    app.put('/api/gk/:app_id', function(req, res) {
+
+        // use our bear model to find the bear we want
+        Apps.findById(req.params.app_id, function(err, app) {
+
+            if (err) res.send(err);
+            //put some data for update here
+            if (req.body.country) app.country = req.body.country;
+            if (req.body.appName) app.appName = req.body.appName;
+            if (req.body.category) app.category = req.body.category;
+            if (req.body.sdpStatus) app.sdpStatus = req.body.sdpStatus;
+            if (req.body.updateTime) app.updateTime = req.body.updateTime;
+            if (req.body.seller) app.seller = req.body.seller;
+            if (req.body.tv) app.tv = req.body.tv;
+            if (req.body.currentStatus) app.currentStatus = req.body.currentStatus;
+            if (req.body.testCycles) app.testCycles = req.body.testCycles;
+            if (req.body.replyTime) app.replyTime = req.body.replyTime;
+            if (req.body.resp) app.resp = req.body.resp;
+            if (req.body._id) app._id = req.body._id; //  $id wont work, so use this id
+
+            // save the bear
+            app.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json(app);
+            });
+
+        });
+    });
+
+    // application -------------------------------------------------------------
+    //add emitter for development and production env
+    app.get('*', function(req, res) {
+        res.render('index.ejs'); // load the single view file (angular will handle the page changes on the front-end)
+    });
 
 
 
