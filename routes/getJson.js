0364d1dc499ff22b7390689c6b1bf46cad154f93
Mekitoji <Mekitojizzz@gmail.com>
2014-09-03 15:49:45 +0400

    rework, bug fix

--- routes/getJson.js
+++ routes/getJson.js
@@ -2,19 +2,12 @@ var url = require("url");
 var queryString = require("querystring");
 
 module.exports = function (app) {
-  app.get('/getJson', function (req, res) {
-
-    // parses the request url
-    var theUrl = url.parse(req.url);
-
-    // gets the query part of the URL and parses it creating an object
-    var queryObj = queryString.parse(theUrl.query);
-
-    // using JSON.parse will parse the jsonData to create an object
-    var obj = JSON.parse(queryObj.jsonData);
-
-    //print obj 
-    res.json(obj);
-    console.log(obj);
+  app.post('/getJson', function (req, res) {
+    console.log(req.body);
+    if (true) {
+      res.json({
+        "result": true
+      });
+    }
   });
 };
