c5ba3498fb9a92473a6349bb2eb4d4e8a833ad2b
Mekitoji <Mekitojizzz@gmail.com>
2014-09-23 18:57:04 +0400

    .

--- public/js/postmail.js
+++ public/js/postmail.js
@@ -4,11 +4,32 @@
  * Post mail to Korea
  */
 angular.module('postmail', [])
-  .factory('mail', function ($http) {
-    return {
-      get: function () {
-        return $http.get();
-      }
-    };
-  })
-  .controller();
+
+.factory('mail', function ($http) {
+  return {
+    post: function () {
+      return $http.post('/postmail', data);
+    }
+  };
+})
+
+.factory('Apps', function ($http) {
+  return {
+    get: function () {
+      return $http.get('/api/gk/');
+    },
+    getApproved: function () {
+      return $http.get('/api/gk/approved');
+    },
+    getRejected: function () {
+      return $http.get('/api/gk/rejected');
+    },
+    getOutdated: function () {
+      return $http.get('/api/gk/outdated');
+    },
+  };
+})
+
+.controller('post', function ($scope, $http, mail) {
+
+});
