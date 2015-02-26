efa35d2bee3d81323110fb50493a30460cf415d4
mktj <Mekitojizzz@gmail.com>
2015-02-27 02:59:02 +0300

    add scripts

--- util/import.js
+++ util/import.js
@@ -5,10 +5,15 @@ var Calendar = require('../models/CIS/calendar');
 var ApprovedCalendar = require('../models/CIS/calendarForApprovedApps');
 var _ = require('lodash');
 
+process.stdin.setRawMode(true);
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+var count = 0;
 
 //util must import all approved apps from approvedApps collection with 2015 year activity to apps collection
 
-//find all calendar value with some activity(storage!==0) 
+//find all calendar value with some activity(storage!==0)
 ApprovedCalendar.find({
   storage: {
     $not: {
@@ -17,31 +22,36 @@ ApprovedCalendar.find({
   }
 })
 
-.exec(function (err, data) {
+.exec(function(err, data) {
   //throw err if not nil
   if (err) throw err;
   //start cool stuff here
-  _.forEach(data, function (n, key) {
-    // console.log("n %s", n);
-    ApprovedApps.findById(n.appId, function (err, app) {
+  _.forEach(data, function(n, key) {
+    ApprovedApps.findById(n.appId, function(err, app) {
       if (err) throw err;
       var privateCheck = false;
       privateCheck = app.applicationId.toString().split("/")[1] ? app.applicationId.toString().split("/")[1] : false;
       if (!privateCheck && n.storage.length !== 0) {
-        app.appId += "/private/" + Math.random().toString().slice(2);
-        Apps.create(app, function (err, res) {
-          if (err) throw err;
+        app.applicationId += "/private/" + Math.random().toString().slice(2);
+
+        Apps.create(app, function(err, res) {
+          if (err) {
+            console.log("App already exist in this collection!");
+            throw err;
+          };
           console.log("App %s succesfully imported from approved collection[%s]", res.appName, res.applicationId)
         });
       }
     })
   });
+});
 
-  process.stdin.resume();
-  //exit
-  process.stdin.on('data', function (key) {
+
+  //exit after all data  receive
+process.stdin.on('data', function(key) {
+  if (key) {
+    process.stdout.write("%s apps was imported.", count);
     process.stdout.write("Goodbye!");
     process.exit();
-  });
-
+  }
 });
--- util/updateKPI.js
+++ util/updateKPI.js
@@ -8,51 +8,100 @@ var ObjectId = require('mongoose').Types.ObjectId;
 var testerStat = require('../models/CIS/testerStat');
 
 
+
+var countReplyTime = function(storage) {
+  var justL = 0,
+    withHiddenL = 0,
+    replyTime = 0;
+  for (var i = 0; i < storage.length; i++) {
+    if (storage[i].value === 'L' || storage[i].value === 'LL') {
+      withHiddenL++;
+    }
+    if (storage[i].value === 'L') {
+      justL++;
+    }
+  }
+  replyTime = withHiddenL / justL;
+  if (replyTime === Infinity || isNaN(replyTime)) {
+    replyTime = 0;
+  }
+  return replyTime;
+};
+
 var count = 0;
 //make request, get all apps
 Apps.find({})
-  .exec(function (err, apps) {
+  .exec(function(err, apps) {
     if (err) throw err;
-    _.forEach(apps, function (n, key) {
+    _.forEach(apps, function(n, key) {
       var responsePerson = n.resp;
       Calendar.findOne({
         "appId": n._id
       })
-        .exec(function (err, res) {
+        .exec(function(err, res) {
           if (err) throw err;
           // console.log(res);
           //if calendar in another collection(approved) res===null
           if (res === null) {
             ApprovedCalendar.findOne({
               "appId": n._id
-            }, function (err, res) {
+            }, function(err, res) {
               if (err) throw err;
               // console.log(res);
               //if res.storage !==0 let add  and check it to kpi
-              if (res !== null) {
+              if (res !== null && res.storage) {
                 if (res.storage.length !== 0) {
                   //TODO
                   testerStat.find({
                     "name": responsePerson
                   })
-                    .exec(function (err, stat) {
+                    .exec(function(err, stat) {
                       if (err) throw err;
-                      console.log(stat);
-                      console.log("Gate one open")
+                      var resStorage = countReplyTime(res.storage)
+                      stat.appStorage.push({
+                        app: new ObjectId(n._id),
+                        year: date.getFullYear(),
+                        testCycle: 1,
+                        respTime: resStorage,
+                        testCycleStorage: [],
+                        respStorage: res.storage
+                      });
+                      //calculate here***********
+                      stat.save(function(err, result) {
+                        if (err) throw err;
+                        console.log("New appStorage created for %s with \"%s\" app", responsePerson, n._id);
+                      })
                     });
-                  console.log(res.storage);
+
+                  // console.log(res.storage);
                 } else {
                   //if len (res.storage) ===0 skip it
                 }
               }
             });
-          } else if (res.storage.length !== 0) {
+          } else if (res.storage && res.storage.length !== 0) {
             //if res.storage !==0 let add  and check it to kpi
-            console.log(res.storage);
-
+            // console.log(res.storage);
+            testerStat.findOne({
+              "name": responsePerson
+            })
+              .exec(function(err, stat) {
+                if (err) throw err;
+                _.forEach(stat.appStorage, function(appStorage) {
+                  if (appStorage.app === n._id) {
+                    appStorage.respStorage = res.storage;
+                    //calculate here**************
+                    appStorage.respStorage = countReplyTime(res.storage);
+                    stat.save(function(err, result) {
+                      if (err) throw err;
+                      console.log("[%s]App storage for %s was updated", responsePerson, n._id);
+                    })
+                  }
+                });
+              });
           } else {
             //if len(res.storage) === 0 skip it
-            console.log("skip")
+            console.log("skip ")
           }
 
         });
