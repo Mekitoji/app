88b16280ca39e6cd876dc118cabe2f81351c997f
Mekitoji <Mekitojizzz@gmail.com>
2015-02-27 11:09:37 +0300

    changes

--- util/import.js
+++ util/import.js
@@ -4,12 +4,13 @@ var ApprovedApps = require('../models/CIS/gkbaseApproved');
 var Calendar = require('../models/CIS/calendar');
 var ApprovedCalendar = require('../models/CIS/calendarForApprovedApps');
 var _ = require('lodash');
+var count = 0;
 
 process.stdin.setRawMode(true);
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
-var count = 0;
+
 
 //util must import all approved apps from approvedApps collection with 2015 year activity to apps collection
 
@@ -22,24 +23,25 @@ ApprovedCalendar.find({
   }
 })
 
-.exec(function(err, data) {
+.exec(function (err, data) {
   //throw err if not nil
   if (err) throw err;
   //start cool stuff here
-  _.forEach(data, function(n, key) {
-    ApprovedApps.findById(n.appId, function(err, app) {
+  _.forEach(data, function (n, key) {
+    ApprovedApps.findById(n.appId, function (err, app) {
       if (err) throw err;
       var privateCheck = false;
       privateCheck = app.applicationId.toString().split("/")[1] ? app.applicationId.toString().split("/")[1] : false;
       if (!privateCheck && n.storage.length !== 0) {
         app.applicationId += "/private/" + Math.random().toString().slice(2);
 
-        Apps.create(app, function(err, res) {
+        Apps.create(app, function (err, res) {
           if (err) {
             console.log("App already exist in this collection!");
             throw err;
           };
           console.log("App %s succesfully imported from approved collection[%s]", res.appName, res.applicationId)
+          count++;
         });
       }
     })
@@ -47,10 +49,10 @@ ApprovedCalendar.find({
 });
 
 
-  //exit after all data  receive
-process.stdin.on('data', function(key) {
+//exit after all data  receive
+process.stdin.on('data', function (key) {
   if (key) {
-    process.stdout.write("%s apps was imported.", count);
+    process.stdout.write(count + " apps was imported.\n");
     process.stdout.write("Goodbye!");
     process.exit();
   }
