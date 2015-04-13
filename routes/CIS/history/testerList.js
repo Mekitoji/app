cbcf916afc4e380537cd4acadd4879184a573650
Mekitoji <Mekitojizzz@gmail.com>
2015-04-13 18:34:34 +0300

    fix testerProfile/List

--- routes/CIS/history/testerList.js
+++ routes/CIS/history/testerList.js
@@ -24,31 +24,35 @@ module.exports = function (app) {
       // res.locals.testers = data.testerStat;
       // res.locals.path = req.path;
       // res.local.apps = data.apps;
-      data.testerStat = _.forEach(data.testerStat, function (n, key) {
-
-        User.findById(n.user)
-
-        .exec(function (err, user) {
-          if (err) res.send(err);
-          n.user = user;
+      if (data === null) {
+        res.json({
+          "Error": "Data not found. Please try to change date"
         });
-        n.appStorage = _.forEach(n.appStorage, function (m, d) {
-          m.app = _.where(data.apps, {
-            "_id": m.app
-          })[0];
-        });
-      });
+      } else {
+        data.testerStat = _.forEach(data.testerStat, function (n, key) {
+          User.findById(n.user)
 
-      setTimeout(function () {
-        res.render('history/testerList', {
-          user: req.user,
-          date: date,
-          testers: data.testerStat,
-          path: req.path,
-          apps: data.apps
+          .exec(function (err, user) {
+            if (err) res.send(err);
+            n.user = user;
+          });
+          n.appStorage = _.forEach(n.appStorage, function (m, d) {
+            m.app = _.where(data.apps, {
+              "_id": m.app
+            })[0];
+          });
         });
-      }, 0);
 
+        setTimeout(function () {
+          res.render('history/testerList', {
+            user: req.user,
+            date: date,
+            testers: data.testerStat,
+            path: req.path,
+            apps: data.apps
+          });
+        }, 500);
+      }
     });
   });
 
@@ -59,6 +63,8 @@ module.exports = function (app) {
     var fDate = new Date(date);
     var nextDay = new Date(date);
     var title = "[" + date + "]" + "Gate Keeper Control History";
+    nextDay.setDate(fDate.getDate() + 1);
+
     History.findOne({
       date: {
         '$gte': fDate,
@@ -68,31 +74,44 @@ module.exports = function (app) {
 
     .exec(function (err, data) {
       if (err) res.send(err);
+      if (data === null) {
+        res.json({
+          "Error": "Data not found. Please try to change date"
+        });
+      } else {
 
-      data.testerStat = _.forEach(data.testerStat, function (n, key) {
+        data.testerStat = _.forEach(data.testerStat, function (n, key) {
 
-        User.findById(n.user)
+          User.findById(n.user)
 
-        .exec(function (err, user) {
-          if (err) res.send(err);
-          n.user = user;
+          .exec(function (err, user) {
+            if (err) res.send(err);
+            n.user = user;
+          });
+
+          n.appStorage = _.forEach(n.appStorage, function (m, d) {
+            m.app = _.where(data.apps, {
+              "_id": m.app
+            })[0];
+          });
+        });
+        data.testerStat = _.find(data.testerStat, function (stat) {
+          // console.log(stat);
+          return stat._id == testerId;
         });
 
-        n.appStorage = _.forEach(n.appStorage, function (m, d) {
-          m.app = _.where(data.apps, {
-            "_id": m.app
-          })[0];
-        });
-      });
-
-      res.render('history/testerProfile', {
-        user: req.user,
-        date: date,
-        title: title,
-        path: req.path,
-        apps: data.apps,
-        tester: data.testerStat
-      });
+        // console.log(data);
+        setTimeout(function () {
+          res.render('history/testerProfile', {
+            user: req.user,
+            date: date,
+            title: title,
+            path: req.path,
+            apps: data.apps,
+            tester: data.testerStat
+          });
+        }, 500)
+      }
     });
   });
 }
