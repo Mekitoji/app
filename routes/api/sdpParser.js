352c9ef2af7ef355cac589f40e94df943f4b92db
Mekitoji <Mekitojizzz@gmail.com>
2015-09-03 13:23:18 +0300

    parser updated

--- routes/api/sdpParser.js
+++ routes/api/sdpParser.js
@@ -1,6 +1,6 @@
-var _ = require("lodash");
+var _          = require("lodash");
 var nodemailer = require('nodemailer');
-var sdp = require('../../models/sdp');
+var sdp        = require('../../models/sdp');
 
 var Apps, ApprovedApps, Cal;
 
@@ -8,7 +8,7 @@ var Apps, ApprovedApps, Cal;
  * Init object with instructions
  * @type {Object}
  */
- // todo: move workspace to db
+// todo: move workspace to db
 var config = {
   headerArr: ['empty', 'appId', 'region', 'gk', 'appName', 'seller', 'updateDate', 'appStatus', 'gkReview'],
   table_caption: "Gate Keeper Review List",
@@ -21,8 +21,12 @@ var config = {
         "EgorovVladimir": "VE",
         "KirillovYury": "YK"
       },
-      email: "vladimir.egorov@lge.com",
-      cc: []
+      mail: {
+        to: '',
+        from: "CIS STE<noreply@lge.com>",
+        cc: [],
+        replyTo: ''
+      }
     },
     "EU": {
       region: ["EU"],
@@ -31,19 +35,35 @@ var config = {
         "TRESCHALOVNIKITA": "NT",
         "KipovskiyEvgeniy": "EK",
         "BelousovAlexey": "AB",
-        "FilimonovMaxim":"MF",
+        "FilimonovMaxim": "MF",
         "SKAKUNGRIGORY": "GS"
       },
-      email: "vladimir.egorov@lge.com",
-      cc: []
+      mail: {
+        to: '',
+        from: "EU STE<noreply@lge.com>",
+        cc: [],
+        replyTo: ''
+      }
     },
     "ALL": {
       region: ["all"],
-      gk: {}
+      gk: {},
+      mail: {
+        to: '',
+        from: "",
+        cc: [],
+        replyTo: ''
+      }
     },
     "SIA": {
       region: ["AA", "HK", "MA", "NA", "TW"],
-      gk: {}
+      gk: {},
+      mail: {
+        to: '',
+        from: "",
+        cc: [],
+        replyTo: ''
+      }
     }
   },
   currentWorkspace: null,
@@ -56,7 +76,7 @@ var utils = {};
  * @param  {[type]}  val value to check
  * @return {Boolean}
  */
-utils.isNullOrUndefined = function(val) {
+utils.isNullOrUndefined = function (val) {
   if (val === null || val === undefined) {
     return true;
   } else {
@@ -69,20 +89,20 @@ utils.isNullOrUndefined = function(val) {
  * @param  {string} Region to parse
  * @return {string|null} Workspace
  */
-utils.checkData = function(region, caption, response) {
+utils.checkData = function (region, caption, response) {
   //get config clean
   config.currentWorkspace = null;
 
-  _.forEach(config.workspace, function(n, key) {
-    _.forEach(n.region, function(r) {
-      if(r === region) {
+  _.forEach(config.workspace, function (n, key) {
+    _.forEach(n.region, function (r) {
+      if (r === region) {
         config.currentWorkspace = key;
         return true;
       }
     });
   });
 
-  if(!utils.isNullOrUndefined(config.currentWorkspace)) {
+  if (!utils.isNullOrUndefined(config.currentWorkspace)) {
     return config.currentWorkspace;
   } else {
     this.responseToClient(response, false, "Can't find this region.");
@@ -98,10 +118,10 @@ utils.checkData = function(region, caption, response) {
  * @param  {String} e      Error message
  * @return void
  */
-utils.responseToClient = function(res, result, msg, e) {
+utils.responseToClient = function (res, result, msg, e) {
   var err;
-  if(process.env.NODE_ENV !== "development") err = null;
-    else err = e ? e : null;
+  if (process.env.NODE_ENV !== "development") err = null;
+  else err = e ? e : null;
   res.json({
     "result": result,
     "msg": msg,
@@ -113,20 +133,20 @@ utils.responseToClient = function(res, result, msg, e) {
  * @param  {String} workspace Current workspace
  * @return void
  */
-utils.interface = function(workspace) {
+utils.interface = function (workspace) {
   if (workspace === "CIS") {
     Apps = require('../../models/CIS/gkbase');
     ApprovedApps = require('../../models/CIS/gkbaseApproved');
     Cal = require('../../models/CIS/calendar');
-  } else if(workspace === "EU") {
+  } else if (workspace === "EU") {
     Apps = require('../../models/EU/gkbase');
     ApprovedApps = require('../../models/EU/gkbaseApproved');
     Cal = require('../../models/EU/calendar');
-  } else if(workspace === "ALL") {
+  } else if (workspace === "ALL") {
     Apps = require('../../models/Sandbox/gkbase');
     ApprovedApps = require('../../models/Sandbox/gkbaseApproved');
     Cal = require('../../models/Sandbox/calendar');
-  } else if(workspace === "SIA") {
+  } else if (workspace === "SIA") {
     Apps = require('../../models/SIA/gkbase');
     ApprovedApps = require('../../models/SIA/gkbaseApproved');
     Cal = require('../../models/SIA/calendar');
@@ -141,168 +161,171 @@ utils.interface = function(workspace) {
  * Get current year
  * @return {Number} Year
  */
-utils.getCurrentYear = function() {
+utils.getCurrentYear = function () {
   var date = new Date();
   return date.getFullYear();
 };
 
 module.exports = function (app) {
-  app.post('/api/getJson', function(req, res) {
-    var region    = req.body.region;
-    var caption   = req.body.table_caption;
-    var rawData   = req.body.data;
+  app.post('/api/getJson', function (req, res) {
+    var region = req.body.region;
+    var caption = req.body.table_caption;
+    var rawData = req.body.data;
     var workspace = utils.checkData(region, caption, res);
-    var data      = JSON.parse(rawData);
-    var p         = [];
+    var data = JSON.parse(rawData);
+    var p = [];
     var requested = [];
     console.log(req.body);
 
     utils.interface(workspace);
 
-    if(utils.isNullOrUndefined(Apps) || utils.isNullOrUndefined(ApprovedApps) || utils.isNullOrUndefined(Cal) || utils.isNullOrUndefined(workspace)) {
+    if (utils.isNullOrUndefined(Apps) || utils.isNullOrUndefined(ApprovedApps) || utils.isNullOrUndefined(Cal) || utils.isNullOrUndefined(workspace)) {
       utils.responseToClient(res, false, "Server error; Workspace is null;", null);
     }
 
-    _.forEach(data, function(arr) {
+    _.forEach(data, function (arr) {
       p.push(_.zipObject(config.headerArr, arr));
     });
 
-    _.forEach(p, function(n) {
+    _.forEach(p, function (n) {
       Apps.findOne({
         applicationId: n.appId
       })
-      .exec(function(err, app) {
-        if (err) utils.responseToClient(res, false, "Server error", err);
+        .exec(function (err, app) {
+          if (err) utils.responseToClient(res, false, "Server error", err);
 
-        // use async lib gajdfajgasdgj!
-        sdp.findOne({id: n.appId},function(err, data){
-          if(err) utils.responseToClient(res, false, "Sdp db request error.", err);
-          if(!data) {
-            sdp.create({
-              id: n.appId,
-              status: n.appStatus
-            }, function(err) {
-              if (err) utils.responseToClient(res, false, "Failed to create new sdp app", err);
-              return;
+          // use async lib gajdfajgasdgj!
+          sdp.findOne({
+            id: n.appId
+          }, function (err, data) {
+            if (err) utils.responseToClient(res, false, "Sdp db request error.", err);
+            if (!data) {
+              sdp.create({
+                id: n.appId,
+                status: n.appStatus
+              }, function (err) {
+                if (err) utils.responseToClient(res, false, "Failed to create new sdp app", err);
+                return;
+              });
+              if (n.appStatus === "Gate Keeper Review Request" || n.appStatus === "Re-GK Review Request") {
+                console.log("pushing " + n.appId + " from new");
+                requested.push({
+                  name: n.appName,
+                  id: n.appId,
+                  status: n.appStatus
+                });
+              }
+            } else {
+              if ((data.status !== "Gate Keeper Review Request" && data.status !== "Re-GK Review Request") &&
+                (n.appStatus === "Gate Keeper Review Request" || n.appStatus === "Re-GK Review Request")) {
+                requested.push({
+                  name: n.appName,
+                  id: n.appId,
+                  status: n.appStatus
+                });
+                data.status = n.appStatus;
+                data.save(function (err) {
+                  if (err) utils.responseToClient(res, false, "Failed to update sdp app", err);
+                });
+              } else if (data.status !== n.appStatus) {
+                data.status = n.appStatus;
+                data.save(function (err) {
+                  if (err) utils.responseToClient(res, false, "Failed to update sdp app", err);
+                });
+              }
+            }
+          });
+
+          var c = config.workspace[config.currentWorkspace];
+          var resp = c.gk[n.gk] ? c.gk[n.gk] : "";
+          if (app === null && n.appStatus !== "App QA approved" || app !== null && app.year !== utils.getCurrentYear()) {
+            var prs = n.seller.split('(');
+            var country = prs[1].slice(0, -1);
+            var seller = prs[0];
+            Apps.create({
+              appName: n.appName,
+              seller: seller,
+              country: country,
+              sdpStatus: n.appStatus,
+              tv: "In Progress",
+              testCycles: 1,
+              updateTime: n.updateDate,
+              replyTime: 0,
+              resp: resp,
+              applicationId: n.appId,
+              year: utils.getCurrentYear()
+            }, function (err, result) {
+              if (err) utils.responseToClient(res, false, "Create app failed. ", err);
+              Cal.create({
+                appId: result._id
+              }, function (err) {
+                if (err) utils.responseToClient(res, false, "Create calendar failed", err);
+                utils.responseToClient(res, true, "Data is succesfully parsed");
+              });
             });
-            if(n.appStatus === "Gate Keeper Review Request" || n.appStatus ==="Re-GK Review Request") {
-              console.log("pushing " + n.appId + "from create");
-              requested.push({
-                id: n.appId,
-                status: n.appStatus
-             });
-            }
+          } else if (app !== null) {
+
+            Apps.findOne({
+              applicationId: n.appId
+            })
+              .exec(function (err, app) {
+                if (err) utils.responseToClient(res, false, "Server error", err);
+                n.updateDate = new Date(n.updateDate);
+                app.appName = n.appName;
+                app.sdpStatus = n.appStatus;
+                app.updateTime = n.updateDate;
+                app.resp = resp;
+                app.save(function (err) {
+                  if (err) utils.responseToClient(res, false, "Failed to save data", err);
+                  utils.responseToClient(res, true, "Apps succesfully updated");
+                });
+              });
           } else {
-            if((data.status !== "Gate Keeper Review Request" && data.status !== "Re-GK Review Request") &&
-              (n.appStatus === "Gate Keeper Review Request" || n.appStatus === "Re-GK Review Request")) {
-              requested.push({
-                id: n.appId,
-                status: n.appStatus
-              });
-              data.status = n.appStatus;
-              data.save(function(err) {
-                if(err) utils.responseToClient(res, false, "Failed to update sdp app", err);
-              });
-            } else if(data.status !== n.appStatus) {
-              data.status = n.appStatus;
-              data.save(function(err) {
-                if(err) utils.responseToClient(res, false, "Failed to update sdp app", err);
-              });
-            }
+            utils.responseToClient(res, true, "QA approved app detected; skip it", null);
+          }
+
+
+        });
+    });
+    setTimeout(function () {
+      console.log(requested);
+      var wspace = config.workspace[config.currentWorkspace];
+      requested.forEach(function (app) {
+        var transport = nodemailer.createTransport();
+        var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        var temp = new Date();
+        var temp_date = temp.getDate();
+        var temp_month = temp.getMonth();
+        var temp_year = temp.getFullYear();
+        var body = "<style>div{font:10pt Arial;}</style>";
+
+        var subject = "[Notification][SDP] " + temp_date + " " + monthArray[temp_month] + " " + temp_year;
+        // **email body
+        body += "<div><b> New apps arrive:</b><br /><br />";
+
+        body += "<b>" + app.name + "[" + app.id + "]</b> with status - <b>" + app.status + "</b><br />";
+
+        body += "<br /><br />Go to <a href='http://localhost:3000/" + config.currentWorkspace + "/2015/rejected#/inwork'>GK Control</a> for more.</div>";
+        // **end of email body
+        var mailOptions = {
+          from: wspace.mail.from,
+          to: wspace.mail.to,
+          cc: wspace.mail.cc,
+          subject: subject,
+          replyTo: wspace.mail.replyTo,
+          text: body,
+          html: body,
+        };
+        console.log(subject);
+        transport.sendMail(mailOptions, function (err, info) {
+          if (err) console.error(err);
+          else {
+            console.log('Message sent: ');
+            console.log(info);
           }
         });
-
-
-        if (app === null && n.appStatus !== "App QA approved" || app !== null && app.year !== utils.getCurrentYear()) {
-          var c = config.workspace[config.currentWorkspace];
-          var resp = c.gk[n.gk] ? c.gk[n.gk] : "";
-          var prs = n.seller.split('(');
-          var country = prs[1].slice(0, -1);
-          var seller = prs[0];
-          Apps.create({
-            appName: n.appName,
-            seller : seller,
-            country: country,
-            sdpStatus: n.appStatus,
-            tv: "In Progress",
-            testCycles: 1,
-            updateTime: n.updateDate,
-            replyTime: 0,
-            resp: resp,
-            applicationId: n.appId,
-            year: utils.getCurrentYear()
-          }, function(err, result){
-            if (err) utils.responseToClient(res, false, "Create app failed. ", err);
-            Cal.create({
-              appId: result._id
-            }, function(err) {
-              if(err) utils.responseToClient(res, false, "Create calendar failed", err);
-              utils.responseToClient(res, true, "Data is succesfully parsed");
-            });
-          });
-        } else if(app !== null) {
-          var c = config.workspace[config.currentWorkspace];
-          var resp = c.gk[n.gk] ? c.gk[n.gk] : "";
-          Apps.findOne({
-            applicationId: n.appId
-          })
-          .exec(function(err, app) {
-            if (err) utils.responseToClient(res, false, "Server error", err);
-
-            n.updateDate   = new Date(n.updateDate);
-            app.appName = n.appName;
-            app.sdpStatus  = n.appStatus;
-            app.updateTime = n.updateDate;
-            app.resp = resp;
-            app.save(function(err) {
-              if(err) utils.responseToClient(res, false, "Failed to save data", err);
-              utils.responseToClient(res, true, "Apps succesfully updated");
-            });
-          });
-        } else {
-          utils.responseToClient(res, true, "QA approved app detected; skip it", null);
-        }
-
-
       });
-    });
-        setTimeout(function() {
-          console.log(requested);
-          var transport = nodemailer.createTransport();
-          var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-          var temp = new Date();
-          var temp_date = temp.getDate();
-          var temp_month = temp.getMonth();
-          var temp_year = temp.getFullYear();
-          var body = "<style>div{font:10pt Arial;}</style>";
 
-          var subject = "[Notification][SDP] " + temp_date + " " + monthArray[temp_month] + " " + temp_year;
-          // **email body
-          body += "<div><b>Test email!!!!</b></div><br /><br />";
-          body += "<div><b> New apps arrive:</b><br /><br />";
-
-          for(var i = 0; i < requested.length; i++) {
-            body +="<b>" + requested[i].id +"</b> with status - <b>" + requested[i].status + "</b><br />";
-          }
-          body += "<br /><br />Go to <a href='http://localhost:3000/cis/2015/rejected#/inwork'>GK Control</a> for more.</div>";
-          // **end of email body
-          var mailOptions = {
-            from: 'CIS STE <noreply@lge.com>',
-            to: 'vladimir.egorov@lge.com',
-            // cc: ['andrey.sayants@lge.com'],
-            subject: subject,
-            replyTo: 'andrey.sayants@lge.com',
-            text: body,
-            html: body,
-          };
-          console.log(subject);
-          transport.sendMail(mailOptions, function (err, info) {
-            if (err) console.error(err);
-            else {
-              console.log('Message sent: ');
-              console.log(info);
-            }
-          });
-        }, 2000);
+    }, 2000);
   });
 };
