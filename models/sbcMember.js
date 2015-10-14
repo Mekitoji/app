4121f68455114745d3e0e8bb5a33ad0b57b8dde6
Mekitoji <mekitojizzz@gmail.com>
2015-10-14 19:50:05 +0300

    init subscribe

---
+++ models/sbcMember.js
@@ -0,0 +1,37 @@
+var mongooose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var member = new Schema({
+  name: {
+    unique: true,
+    type: String
+  },
+  mail: {
+    unique: true,
+    type: String
+  }
+});
+
+
+member.statics.add = function(name, mail, cb) {
+    this.create({
+    name: name,
+    mail: mail
+  });
+};
+
+member.statics.removeByName = function(name, cb) {
+  this.findOneAndRemove({
+    name: name
+  }, cb);
+};
+
+member.statics.removeByMail = function(mail, cb) {
+  this.findOneAndRemove({
+    mail: mail
+  }, cb);
+};
+
+var sbcMember = mongoose.model('sbcMember', member);
+
+module.exports = sbcMember;
--- models/sdp.js
+++ models/sdp.js
@@ -3,7 +3,23 @@ var Schema = mongoose.Schema;
 
 var SdpStats = new Schema({
   id: String,
-  status: String
+  status: String,
+  subscribers: [{
+    type:Schema.ObjectId,
+    ref: 'sbcMember'
+  }]
 });
+
+SdpStats.static.add = function(id, status) {
+  this.create({
+    id: id,
+    status: status
+  });
+};
+
+SdpStats.methods.subscribe = function(sub, cb) {
+  this.subcribers.push(sub);
+  this.save(cb);
+};
 
 module.exports = mongoose.model('Sdp', SdpStats);
