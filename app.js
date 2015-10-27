fe44441b79d2c5a86ab8802c76ef3d7fe8d3dece
Mekitoji <mekitojizzz@gmail.com>
2015-10-27 17:53:07 +0300

    fixing 'Request entity too large'

--- app.js
+++ app.js
@@ -47,8 +47,8 @@ app.set('view engine', 'ejs');
 
 //set up express application
 app.use(favicon());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
