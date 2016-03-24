a22bacce6f3518955cbdda3133e8469f63760d13
Mekitoji <mekitojizzz@gmail.com>
2016-03-24 13:15:08 +0300

    fix filter

--- public/js/sdpSubscribe.js
+++ public/js/sdpSubscribe.js
@@ -28,6 +28,37 @@ angular.module('sdpSubscribe', ['ngSanitize', 'ui.select'])
     };
 })
 
+.filter('propertyFilter', function($log) {
+ return function(items, props) {
+    var out = [];
+    if (angular.isArray(items)) {
+    items.forEach(function(item) {
+        var itemMatches = false;
+        var keys = Object.keys(props);
+        var optionValue = '';
+        for (var i = 0; i < keys.length; i++) {
+             optionValue = item[keys[i]] ? optionValue + item[keys[i]].toString().toLowerCase().replace(/ /g, '') : '';
+        }
+        for (var j = 0; j < keys.length; j++) {
+            var text = props[keys[j]].toLowerCase().replace(/ /g, '');
+            if (optionValue.indexOf(text) !== -1) {
+               itemMatches = true;
+               break;
+            }
+        }
+        if (itemMatches) {
+            out.push(item);
+        }
+        });
+        } else {
+            // Let the output be the input untouched
+            out = items;
+        }
+
+        return out;
+    };
+})
+
 .controller('subscribe', function($scope, Subscribe, Members) {
   function sortName(a, b) {
     // test localeCompate
@@ -73,8 +104,11 @@ angular.module('sdpSubscribe', ['ngSanitize', 'ui.select'])
 
   });
 
-  $scope.submit = function() {
+  $scope.$watch('newAppSubId.select', function(n) {
+    console.log(n);
+  });
 
+  $scope.submit = function() {
     console.dir({
       id: $scope.newAppSubId.select._id,
       story: $scope.form.story,
